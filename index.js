b091e2ee94e17ef9026f1e70d897e607a23ed049
Evgeniy <evgeniy1demyanov@gmail.com>
2022-06-03 09:17:39 +0300

    Add reference.

--- index.js
+++ index.js
@@ -74,3 +74,5 @@ setInterval(() => {
   rotate(scaled, sMatrix, r, elCenter);
   r += 0.1;
 }, 150)
+
+// https://ru.stackoverflow.com/questions/970063/%D0%92%D1%80%D0%B0%D1%89%D0%B5%D0%BD%D0%B8%D0%B5-svg-%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%B0-%D0%B2%D0%BE%D0%BA%D1%80%D1%83%D0%B3-%D1%86%D0%B5%D0%BD%D1%82%D1%80%D0%B0
